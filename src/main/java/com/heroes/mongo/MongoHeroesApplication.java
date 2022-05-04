b31603942c516eda627ebff647620c46ba9f2888
JonasGarciaGit <=>
2022-05-04 20:57:51 -0300

    Projeto criado

---
+++ src/main/java/com/heroes/mongo/MongoHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.heroes.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoHeroesApplication.class, args);
+	}
+
+}
